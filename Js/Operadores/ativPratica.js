e7763d7286dd3f81da978a04351023a7db264092
JpLimags <joaopedro66.fortal@gmail.com>
2022-06-30 20:16:26 -0300

    Projeto alterado com sucesso

--- Js/Operadores/ativPratica.js
+++ Js/Operadores/ativPratica.js
@@ -1,9 +1,9 @@
 function comparaNumeros (n1, n2){
 
-    const primeiraFrase = firstFrase(num1, num2);
-    //const segundaFrase = segundaFrase(num1, num2); 
+    const primeiraFrase = firstFrase(n1, n2);
+    const segundaFrase = secondFrase(n1, n2); 
 
-    return `${primeiraFrase}`;
+    return `${primeiraFrase} ${segundaFrase}`;
 }
 
 
@@ -13,17 +13,27 @@ function firstFrase(n1,n2){
 
     (n1 == n2) ? TrueOrfalse = 'iguais' : TrueOrfalse = 'diferentes';
 
-    return `Os valores de ${n1} e ${n2} são ${TrueOrfalse}`
+    return `Os valores de ${n1} e ${n2} são ${TrueOrfalse}`;
 };
+
 
 function secondFrase(n1,n2){
 
-    let TrueOrfalse = '';
+    var comp10 = 'menor';
+    var comp20 = 'menor';
 
-    (n1 == n2) ? TrueOrfalse = 'iguais' : TrueOrfalse = 'diferentes';
+    if((n1+n2) > 10){
 
-    return `Os valores de ${n1} e ${n2} são ${TrueOrfalse}`
+        comp10 = 'maior';
+    }
+
+    if((n1+n2) > 20){
+
+        comp20 = 'maior'
+    }
+
+    return `Sua soma ${n1+n2}, é ${comp10} que 10, e ${comp20} que 20`;
 };
 
-console.log(comparaNumeros(5,6));
 
+console.log(comparaNumeros(5,10));
