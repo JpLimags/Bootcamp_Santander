a259b8626054049ee8988f7731654e1dd8b216dc
JpLimags <joaopedro66.fortal@gmail.com>
2022-07-02 08:00:05 -0300

    Inclusão de código

---
+++ Js/VariaveisTipos/const.js
@@ -0,0 +1,7 @@
+const FIRST_NAME = "João Pedro "
+
+//FIRST_NAME = "Jp";
+/*O código acima não pode acontecer pois quando declaramos uma constante estamos declarando algo imutável
+e que mantém seu valor por todo o programa*/
+
+console.log(FIRST_NAME);
---
+++ Js/VariaveisTipos/hosting.js
@@ -0,0 +1,8 @@
+// Exemplo sobre hosting
+
+number1 = 1;
+
+
+console.log(number1 + 2);
+
+var number1;
