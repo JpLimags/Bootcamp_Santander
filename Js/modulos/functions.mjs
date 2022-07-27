3f6cadec9601e3e8867e0286784324d66e1cf2b9
JpLimags <joaopedro66.fortal@gmail.com>
2022-07-27 10:55:41 -0300

    Adicionando novos exercicíos

---
+++ Js/modulos/functions.mjs
@@ -0,0 +1,24 @@
+function mostraIdade(nome, idade){
+
+
+    return `A idade de ${nome} é ${idade}`
+}
+
+function mostraCidade(nome, cidade){
+
+
+    return `O ${nome} mora em ${cidade}`
+}
+
+function mostraHobby(nome, hobby){
+
+
+    return `O ${nome} tem o hobby de ${hobby}`
+}
+
+export{
+
+    mostraCidade,
+    mostraIdade,
+    mostraHobby
+}
---
+++ Js/modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./functions.mjs";
+
+console.log(mostraIdade("Jp", 19));
+console.log(mostraCidade("Jp", "Fortaleza"));
+console.log(mostraHobby("Jp", "Desenhar"))
